5744ef50d9c9f77fee1b070c66ec5e6f50997d43
dagarcia522 <dagarcia522@ucatolica.edu.co>
2026-08-18 11:50:03 -0500

    Ejercicio 4,2 Prueba 1

--- src/data/data.py
+++ src/data/data.py
@@ -65,7 +65,23 @@ class Data:
         Returns:
             list: Lista combinada y ordenada
         """
-        pass
+        resultado = []
+        i = 0
+        j = 0
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+        while i < len(lista1):
+            resultado.append(lista1[i])
+            i += 1
+        while j < len(lista2):
+            resultado.append(lista2[j])
+            j += 1
+        return resultado
     
     def rotar_lista(self, lista, k):
         """
