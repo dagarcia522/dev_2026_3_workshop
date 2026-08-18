ca399e2b29c704329e513bacfe2272076bd5cd8e
dagarcia522 <dagarcia522@ucatolica.edu.co>
2026-08-17 23:10:10 -0500

    Codigo conversion Ejer10, Prueba 1

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -197,4 +197,21 @@ class Conversion:
             morse_a_texto("... --- ...") -> "SOS"
             morse_a_texto(".... . .-.. .-.. ---") -> "HELLO"
         """
-        pass
+        morse_sig = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+    '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+    '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+    '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+    '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+    '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4',
+    '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '/': ' '}
+        codigo = morse.strip().split('   ')
+        resultado = []
+        tokens = morse.split(' ')
+        for token in tokens:
+            if token in morse_sig:
+                resultado.append(morse_sig[token])
+            elif token == '':
+                resultado.append(' ')
+        return "".join(resultado)
+        
